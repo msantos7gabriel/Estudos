ef73d3b0009097a058b42139ad33c81e87aa085c
Ms7Biel <gabriel.msantos7@hotmail.com>
2024-12-06 09:36:50 -0300

    segunda parte da olimpiada

---
+++ Python/Maratona IF/segunda parte/codigo.py
@@ -0,0 +1,4 @@
+n = int(input())
+nums = list(map(str, input().split()))
+test = ''.join(nums)
+print(test.count('100'))
---
+++ Python/Maratona IF/segunda parte/domino.py
@@ -0,0 +1,7 @@
+n = int(input())
+peças = 0
+
+for i in range(n+2):
+    peças += i
+
+print(peças)
