a18eb9b4aed3d7cf65928577b1dc5f6f6588fca6
Ms7Biel <gabriel.msantos7@hotmail.com>
2024-12-09 23:55:12 -0300

    Estudos de Django do dia 9-12-2024

--- Python/Django/Login diferente/users/forms.py
+++ Python/Django/Login diferente/users/forms.py
@@ -1,5 +1,6 @@
 # from django.contrib.auth.models import User
 from django import forms
+from django.core.exceptions import ValidationError
 
 
 # class LoginForm(forms.ModelForm):
@@ -12,11 +13,20 @@ class LoginForm(forms.Form):
     login = forms.CharField(max_length=30)
     senha = forms.CharField(max_length=30, widget=forms.PasswordInput)
 
-
     # Se eu precisar pegar dois campos ou mais ao mesmo tempo
     # utilizo a função  da seguinte maneira
-"""    def clean(self):
-        cleaned_data = super().clean()
-        nome = cleaned_data.get('login')   
-        senha = cleaned_data.get('senha')   
-"""
+    """    def clean(self):
+            cleaned_data = super().clean()
+            nome = cleaned_data.get('login')   
+            senha = cleaned_data.get('senha')   
+
+            VALIDAÇÃO
+    """
+
+    def clean_login(self):
+        nome = self.cleaned_data['login']
+
+        if not (nome.isalnum()):
+            raise ValidationError(
+                'O nome de Usuário não pode ter caractere especial')
+        return nome
--- Python/Django/Login diferente/users/views.py
+++ Python/Django/Login diferente/users/views.py
@@ -7,17 +7,24 @@ from django.contrib.auth import login as authLogin
 
 
 def login(request):
+    error = False
     if request.method != 'POST':
         form = LoginForm()
     else:
         form = LoginForm(data=request.POST)
         if form.is_valid():
-            username = request.POST.get('username')
-            password = request.POST.get('password')
-            user = authenticate(username=username, password=password)
+            # Usados no método comentado no forms 
+            # username = request.POST.get('username')
+            # password = request.POST.get('password')
+            login = request.POST.get('login')
+            senha = request.POST.get('senha')
+            user = authenticate(username=login, password=senha)
 
             if user:
                 authLogin(request, user)
                 return HttpResponseRedirect(reverse('index'))
-    context = {'form': form}
+            else:
+                error = True
+
+    context = {'form': form, 'error': error}
     return render(request, 'users/login.html', context)
