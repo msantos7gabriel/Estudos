3b32bb0e2342ceb17241f168541dca9fb50e3c4b
Ms7Biel <gabriel.msantos7@hotmail.com>
2024-12-19 12:41:31 -0300

    Trem de Livia

--- Python/Projetos/tremdelivia.py
+++ Python/Projetos/tremdelivia.py
@@ -2,34 +2,58 @@ import pyautogui as py
 import pandas as pd
 from time import sleep
 from random import randint
+import argparse
+
+parser = argparse.ArgumentParser(description='...')
+parser.add_argument('--browser', help="user fev browser", default="opera")
+parser.add_argument('--resx', help="lat and long of A point", default=1920)
+parser.add_argument('--resy', help="lat and long of A point", default=1080)
+args = parser.parse_args()
+
+browser = args.browser
+resx = int(args.resx)
+resy = int(args.resy)
 
 
-def abrir_opera():
+def res_conversorx(cordx):
+    return cordx * resx / 1920
+
+
+def res_conversory(cordy):
+    return cordy * resy / 1080
+
+
+def abrir_browser():
     py.press('win')
-    py.write('opera')
+    py.write(browser)
     sleep(1)
     py.press('enter')
-    sleep(5)
+    sleep(6)
 
 
 def votar():
-    sleep(2)
-    py.hotkey('ctrl', 'shift', 'n')
-    sleep(1.5)
-    py.write('https://pollie.app/1pkqv', interval=0.1)
-    py.press('enter')
-    sleep(5)
-    py.click(900, 925)
+    # Abrir nova aba anonima
     sleep(1)
-    py.press('tab')
-    nome = tabelas()
-    py.write(nome, interval=0.1)
-    py.press('enter')
-    sleep(3.5)
-    py.click(800, 900)
+    py.hotkey('ctrl', 'shift', 'n')
     sleep(0.5)
-    py.click(1800, 1020)
+    # botar a url para o voto
+    py.write('https://pollie.app/xa7tf')
+    py.press('enter')
+    # carregando a pagina
+    sleep(4.5)
+    # clicar no botão de concordar com os cookies
+    py.click(res_conversorx(900), res_conversory(925))
+    # Escreve o nome
+    py.press('tab')
+    # Escreve um nome aleatório
+    nome = tabelas()
+    py.write(nome)
+    py.press('enter')
     sleep(2)
+    # Clica no botão de votar
+    py.click(res_conversorx(1800), res_conversory(1020))
+    sleep(0.3)
+    # Fecha a aba
     py.hotkey('ctrl', 'w')
 
 
@@ -47,7 +71,8 @@ def tabelas():
 # Inicialização
 qtd_votos = int(input('Quantos votos deseja realizar? '))
 votos_realizados = 0
-abrir_opera()
+abrir_browser()
+
 for _ in range(qtd_votos):
     try:
         votar()
