01c28592d9093257660c29808c27543b2a4b97fc
Gabriel Montalvão Santos <gabriel.msantos7@hotmail.com>
2024-12-28 18:37:53 -0300

    Django Estudos

--- Python/Django/users/urls.py
+++ Python/Django/users/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('login', auth_views.LoginView.as_view(
         template_name='users/login.html'), name='login'),
     path('logout', views.logout_view, name='logout'),
+    path('register', views.register, name='register'),
+    
 ]
