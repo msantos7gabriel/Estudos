84a8ab32af38cc5bf5ab4d584b893178f1bc9653
Gabriel Montalvão Santos <gabriel.msantos7@hotmail.com>
2025-01-22 08:55:18 -0300

    Crud completado

--- Python/Django/StudyBud/base/models.py
+++ Python/Django/StudyBud/base/models.py
@@ -1,9 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Topic(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
 
 
 class Room(models.Model):
-    # host =
-    # topic =
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
     # participants =
@@ -12,5 +20,21 @@ class Room(models.Model):
     # Salva ele uma vez que foi instanciado e não irá mudar
     created = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        ordering = ['-updated', 'created']
+
     def __str__(self):
         return str(self.name)
+
+
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    body = models.TextField()  # Corpo da Mensagem
+    # Salva o valor toda vez quem voce salva ele
+    updated = models.DateTimeField(auto_now=True)
+    # Salva ele uma vez que foi instanciado e não irá mudar
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.body[0:50]
